b0ab2b30afb7eb3a29e362f14ae863c1906fbc8c
prabeshnpl <nepalprabesh99@gmail.com>
2025-07-09 12:02:37 +0545

    added urls for pk and refresh token

--- trackerapi/tracker_api/urls.py
+++ trackerapi/tracker_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Register, Login, Expenses
+from .views import Register, Login, Expenses, Logout
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('auth/register/',Register.as_view(),name='register'),
     path('auth/login/',Login.as_view(),name='login'),
+    path('auth/logout/',Logout.as_view(),name='logout'),
     path('auth/refresh/',TokenRefreshView.as_view(),name='refresh'),
 
     path('expenses/',Expenses.as_view(), name='expenses'),
